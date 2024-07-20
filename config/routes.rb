9b7831da3ad7b7e51f9b39128a407fa153a70994
Oxana Michkasova <oxana.mitch@gmail.com>
2024-07-19 22:19:42 -0400

    Set up default page (index) in routes,  add navigation and basic styling for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+
 Rails.application.routes.draw do
+  root 'forums#index'
   # resources :users
   post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
   delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
