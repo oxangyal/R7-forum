d0a1512e8e2a690fbd55d2ed54cd0765eeb534c9
Oxana Michkasova <oxana.mitch@gmail.com>
2024-07-23 22:46:48 -0400

    Create routes and post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 
 Rails.application.routes.draw do
   root 'forums#index'
+
+  get 'posts/create'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
+  
   # resources :users
   post '/users/:id/logon', to: 'users#logon', as: 'user_logon'
   delete '/users/logoff', to: 'users#logoff', as: 'user_logoff'
@@ -12,6 +20,11 @@ Rails.application.routes.draw do
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#delete'
   resources :forums
+  resources :forums do
+    resources :posts, shallow: true, except: [:index]
+    resources :subscriptions, shallow: true, except: [:index]
+  end
+  get '/subscriptions', to: 'subscriptions#index', as: 'subscriptions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
